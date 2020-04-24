c954097fd80407852d20f8e24f59140a24830f36
LyhourChhen <ukamilove66@gmail.com>
2020-04-24 09:17:52 +0700

    refector project funtion with custome

--- xylophone/lib/main.dart
+++ xylophone/lib/main.dart
@@ -9,6 +9,20 @@ class XylophoneApp extends StatelessWidget {
     assetsAudioPlayer.open(Audio("assets/note$sound_number.wav"));
   }
 
+  Expanded playSoundUI({Color color, int sound}) {
+    return Expanded(
+      child: FlatButton(
+        padding: EdgeInsets.all(0),
+        onPressed: () {
+          playSound(sound);
+        },
+        child: Container(
+          color: color,
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -19,83 +33,13 @@ class XylophoneApp extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Expanded(
-                    child: FlatButton(
-                      padding: EdgeInsets.all(0),
-                      onPressed: () {
-                        playSound(1);
-                      },
-                      child: Container(
-                        color: Colors.red,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: FlatButton(
-                      padding: EdgeInsets.all(0),
-                      onPressed: () {
-                        playSound(2);
-                      },
-                      child: Container(
-                        color: Colors.blue,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: FlatButton(
-                      padding: EdgeInsets.all(0),
-                      onPressed: () {
-                        playSound(3);
-                      },
-                      child: Container(
-                        color: Colors.yellow,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: FlatButton(
-                      padding: EdgeInsets.all(0),
-                      onPressed: () {
-                        playSound(4);
-                      },
-                      child: Container(
-                        color: Colors.green,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: FlatButton(
-                      padding: EdgeInsets.all(0),
-                      onPressed: () {
-                        playSound(5);
-                      },
-                      child: Container(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: FlatButton(
-                      padding: EdgeInsets.all(0),
-                      onPressed: () {
-                        playSound(6);
-                      },
-                      child: Container(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    child: FlatButton(
-                      padding: EdgeInsets.all(0),
-                      onPressed: () {
-                        playSound(7);
-                      },
-                      child: Container(
-                        color: Colors.purple,
-                      ),
-                    ),
-                  ),
+                  playSoundUI(color: Colors.red, sound: 1),
+                  playSoundUI(color: Colors.blue, sound: 2),
+                  playSoundUI(color: Colors.green, sound: 3),
+                  playSoundUI(color: Colors.purple, sound: 4),
+                  playSoundUI(color: Colors.white, sound: 5),
+                  playSoundUI(color: Colors.black, sound: 6),
+                  playSoundUI(color: Colors.blue, sound: 7),
                 ],
               ),
             ),
