a0e85ea9abe3f0405b39ef03ba370319b4856212
LyhourChhen <ukamilove66@gmail.com>
2020-05-08 14:52:04 +0700

    update callback in flutter

--- todoey/lib/components/todo_tile_view.dart
+++ todoey/lib/components/todo_tile_view.dart
@@ -6,15 +6,46 @@
 
 import "package:flutter/material.dart";
 
-class TodoListTiles extends StatelessWidget {
+class TodoListTiles extends StatefulWidget {
+  @override
+  _TodoListTilesState createState() => _TodoListTilesState();
+}
+
+class _TodoListTilesState extends State<TodoListTiles> {
+  bool isCheck = true;
+
+  void handleCheck(value) {
+    print("click me");
+    setState(() {
+      isCheck = !isCheck;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text("Finished Flutter Course on Udemy"),
-      trailing: Checkbox(
-        onChanged: (value) => print(value),
-        value: false,
+      title: Text("Finished Flutter Course on Udemy",
+          style: TextStyle(
+            decoration: isCheck ? TextDecoration.lineThrough : null,
+          )),
+      trailing: MyCheckbox(
+        checkboxstate: isCheck,
+        handleCheck: handleCheck,
       ),
+    );
+  }
+}
+
+class MyCheckbox extends StatelessWidget {
+  MyCheckbox({this.checkboxstate, this.handleCheck});
+  final bool checkboxstate;
+  final Function handleCheck;
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: checkboxstate,
+      onChanged: handleCheck,
     );
   }
 }
